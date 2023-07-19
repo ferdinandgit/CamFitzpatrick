69949345d0e7ad1be5f07f5bffa0d7351bca9c96
Ferdix <80453736+ferdinandgit@users.noreply.github.com>
2023-07-19 15:06:37 +0200

    patch try csv file error

--- skintone.py
+++ skintone.py
@@ -127,21 +127,6 @@ def rgbToLab(inputColor):
     return Lab
 
 
-
-"""def estimateBrg(img):
-    bleu = []
-    red = []
-    green = []
-    wid,lgt,scale=np.shape(img)
-    for i in range(wid):
-        for j in range(lgt):
-            if img[i][j][0]!=0 and img[i][j][1]!=0 and img[i][j][2]!=0:
-                bleu.append(img[i][j][0])
-                red.append(img[i][j][1])
-                green.append(img[i][j][2])
-    return [np.mean(bleu),np.mean(red),np.mean(green)]"""
-
-
 def estimateBrg(img):
     
     """Estimation of the global color of an image.
@@ -218,29 +203,31 @@ mesureredlist=[] #store red measure of each pixel from the camera
 mesuregreenlist=[] #store green measure of each pixel from the camera
 mesurebleulist=[] #store bleu measure of each pixel from the camera
 
-#read the csv file with references and measures from the camera. Calibration csv is composed of 6 rows, 3 for RGB reference, 3 for RGB measure.  
-with open('calibration.csv', newline='') as csvfile:
-    reader = csv.DictReader(csvfile)
-    for row in reader:
-        storecsv.append([row['R_ref'],row['G_ref'],row['B_ref'], row['R'],row['G'],row['B']])
-
-#Remove the column descriptor of the csv file.
-storecsv.pop(1)
-
-#Store value for interpolation calculation 
-for couple in storecsv:
-    gammared.append(float(couple[0])/float(couple[3]))
-    gammagreen.append(float(couple[1])/float(couple[4]))
-    gammableu.append(float(couple[2])/float(couple[5]))
-    mesureredlist.append(float(couple[3]))
-    mesuregreenlist.append(float(couple[4]))
-    mesurebleulist.append(float(couple[5]))
-
-#determine R,G,B correction functions
 try:
+    #read the csv file with references and measures from the camera. Calibration csv is composed of 6 rows, 3 for RGB reference, 3 for RGB measure.  
+    with open('calibration.csv', newline='') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            storecsv.append([row['R_ref'],row['G_ref'],row['B_ref'], row['R'],row['G'],row['B']])
+
+    #Remove the column descriptor of the csv file.
+    storecsv.pop(1)
+
+    #Store value for interpolation calculation 
+    for couple in storecsv:
+        gammared.append(float(couple[0])/float(couple[3]))
+        gammagreen.append(float(couple[1])/float(couple[4]))
+        gammableu.append(float(couple[2])/float(couple[5]))
+        mesureredlist.append(float(couple[3]))
+        mesuregreenlist.append(float(couple[4]))
+        mesurebleulist.append(float(couple[5]))
+
+    
+    #determine R,G,B correction function
     fbleu=scipy.interpolate.interp1d(mesurebleulist,gammableu,bounds_error=False,fill_value="extrapolate")
     fgreen=scipy.interpolate.interp1d(mesuregreenlist,gammagreen,bounds_error=False,fill_value="extrapolate")
     fred=scipy.interpolate.interp1d(mesureredlist,gammared,bounds_error=False,fill_value="extrapolate")
+
 except:
     print("Check your calibration csv file")
     print("The script will run without calibration")
